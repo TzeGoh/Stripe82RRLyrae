f4753224f5579f36458f0597947677c876bb3cd6
Astro-phyTZEcist <tpg2107@columbia.edu>
2016-03-26 02:20:14 -0400

    Update Give_me_with_down_sample.py

--- scripts/Give_me_with_down_sample.py
+++ scripts/Give_me_with_down_sample.py
@@ -2,25 +2,53 @@
 #
 # What: Run_me_with_down_sample.py
 #
-# How : This is the main python file where you run the functions for down-sampling light
-#       curves. The goal is to get n number of best periods   
+# Pair: Give_me_n_best_periods_Function.py
 # 
 # Date : Mar 14, 2016
 ##########################################################################################
 
+'''
+Utilises a function to run down sampled light curves and give you 'n' best periods
+	
+Parameters
+----------   
+i : <type 'int'>
+    The index number of light curve you want to examine
+    	
+model : <class 'gatspy.periodic.lomb_scargle_multiband.LombScargleMultiband'>
+    The Lomb Scargle fit model you want
+    	
+n : <type 'int'>
+    How many best fit periods you want 
+    
+d_array : <type 'numpy.ndarray'> of <type 'int'>	
+    A numpy array of numbers of the powers of base 2...up to and including nobs 
 
-from gatspy import periodic
+Returns
+-------
+l_cruves : <type 'list'> containing <type 'tuple'>  
+    Each tuple will contain : 
+    
+returned_list[index][0] : snobs : <type 'int'>
+    The Number of Observations/Nights that has been downsampled
+    	
+returned_list[index][1] : tze_periods : <type 'numpy.ndarray'> of <type 'numpy.float32'>
+    A numpy array of the 'n' best periods 
+    			
+returned_list[index][2] : metadata['P'] : <type 'numpy.float32'>	
+    The system's best average period ie Vandplas periods
+    	
+'''
 
-# This Function returns will give you a light curve with the n best periods
+# Project
 import Give_me_n_best_periods_Function as Give_Func
-
-print("The file runs")
+from gatspy import periodic
 
 # This is my fitting parameters :
 nterms_base=2
 nterms_band=1
 
-# This is it, this is your LombScargle code ! Pick one of the 2 below
+# This is your LombScargle code ! Pick one of the 2 below
 model = periodic.LombScargleMultiband(Nterms_base=nterms_base, Nterms_band=nterms_band)
 ##model = periodic.SuperSmoother(fit_period=True)
 
@@ -36,14 +64,11 @@ def main():
 	for i in list_of_index:
 		# The number of observations
 		nobs = Give_Func.give_me_nobs(i,model,n)
-		print(nobs)
 		
 		# The Down sampled array
 		d_array = Give_Func.Give_Me_Down_Sample(nobs)
 		
-		# It returns this : No. observations, 'n' number of periods, and system's best ave
+		# Returns No. observations, 'n' number of periods, and system's best ave
 		l_curves = Give_Func.Give_Me_Light_Curves_Down_Sampled(i,nobs,model,n,d_array)
-		print(l_curves)
-		print('The main loop works')
     	
 main()
